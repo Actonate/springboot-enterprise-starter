478da71aca3e5b93ef04dbe609954881fe1cf4a0
Piyush Patel <prpateldev@gmail.com>
2022-05-05 14:33:17 +0530

    adding flyway

---
+++ src/main/java/com/accio/spring/starter/configs/EmptyMigrationStrategyConfig.java
@@ -0,0 +1,17 @@
+package com.accio.spring.starter.configs;
+
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmptyMigrationStrategyConfig {
+
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            // do nothing
+        };
+    }
+}
